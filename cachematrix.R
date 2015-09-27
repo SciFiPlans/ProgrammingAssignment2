e1a127468c8971d4061ca8117076f085f66fb9f2
Luis Vilasa <luis.vilasa@gmail.com>
2015-09-27 19:32:19 +0200

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two functions make use of the scoping rules in R. 
+## A matrix made of functions is created, a second function makes use of the first one in order to:
+## Check if the inverse of a matrix is already calculated, if not it calculates it
 
-## Write a short comment describing this function
+## This function creates a fake matrix from a list of functions which do the following: 
+#Set the value of a matrix
+#Get the value of a matrix
+#Set the value of the inverse of a matrix
+#Get the value of the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) inv <<- solve
+  getmatrix <- function() inv
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function check if the inverse of a matrix is already computed and if not computes it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getmatrix()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setmatrix(inv)
+  inv
 }
